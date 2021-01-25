03027d773ee6f88d7e1b17ef8ccaa4e1b007c011
XingYun-Cloud <3933074@qq.com>
2021-01-25 16:20:08 +0800

    .

---
+++ Tools/SysWhispers-master/syscalls_mem.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Windows.h>
+
+EXTERN_C NTSTATUS NtWriteVirtualMemory(
+	IN HANDLE ProcessHandle,
+	IN PVOID BaseAddress,
+	IN PVOID Buffer,
+	IN SIZE_T NumberOfBytesToWrite,
+	OUT PSIZE_T NumberOfBytesWritten OPTIONAL);
+
+EXTERN_C NTSTATUS NtProtectVirtualMemory(
+	IN HANDLE ProcessHandle,
+	IN OUT PVOID * BaseAddress,
+	IN OUT PSIZE_T RegionSize,
+	IN ULONG NewProtect,
+	OUT PULONG OldProtect);
+
